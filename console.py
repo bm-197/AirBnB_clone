f51d70c98c664ba33a21a9d55bf2771ab13cb64e
ZorEl212 <yabsirad212@gmail.com>
2023-10-11 07:04:23 -0400

    Console: Refactor the code a little bit 	Fix prompt for better output (as it is in the requirment)

--- console.py
+++ console.py
@@ -6,14 +6,17 @@ from models import storage
 from models.base_model import BaseModel
 
 def convert(line):
+    """Split input from tty in with whitespace as a delimiter
+        Args:
+            line - tty input stream"""
     line_2 = line.split(" ")
     return line_2
 
 class HBNBCommand(cmd.Cmd):
     """Defines the HolbertonBnB command interpreter."""
 
-    prompt = "(hbnb)"
-    __classes = {"BaseModel"}
+    prompt = "(hbnb) "
+    __classes = {"BaseModel": BaseModel}
 
     def do_quit(self, line):
         """Quit command to exit the progrma."""
@@ -23,6 +26,10 @@ class HBNBCommand(cmd.Cmd):
         """EOF signal to exit program."""
         print("")
         return True
+
+    def emptyline(self):
+        """Do nothing if no commnd is provided"""
+        return
     
     def do_create(self, line):
         """Create a new class instance and print its id."""
@@ -31,12 +38,11 @@ class HBNBCommand(cmd.Cmd):
         if len(line_2) == 0:
             print("** class name missing **")
         
-        elif line_2[0] not in HBNBCommand.__classes:
+        elif line_2[0] not in self.__classes:
             print("** class doesn't exit **")
-        
-        else:
-            print(eval(line_2[0])().id)
-            storage.save()
+    
+        print(self.__classes[line_2[0]]().id)
+        storage.save()
     
     def do_show(self, line):
         """Display the string representation of a class instance of a given id."""
