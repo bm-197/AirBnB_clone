a8311158a2ce98fd96ee04a57670d3867d378159
ZorEl212 <yabsirad212@gmail.com>
2023-10-11 09:19:05 -0400

    Revert: Console

--- console.py
+++ console.py
@@ -6,22 +6,12 @@ import models
 from models.base_model import BaseModel
 
 def convert(line):
-<<<<<<< HEAD
-<<<<<<< HEAD
-    line_2 = []
-    if len(line) != 0:
-        line_2 = line.split(" ")
-=======
     """Split input from tty in with whitespace as a delimiter
         Args:
             line - tty input stream"""
     if len(line) == 0:
         return []
     line_2 = line.split(" ")
->>>>>>> d786d428a946a8ec02ee9945d6fd562b316e63ca
-=======
-    line_2 = line.split(" ")
->>>>>>> parent of 29362e0... Added update method for the console app
     return line_2
 
 class HBNBCommand(cmd.Cmd):
@@ -97,84 +87,23 @@ class HBNBCommand(cmd.Cmd):
     
     def do_all(self, line):
         """Display string representations of all instances of a given class."""
-        line_2 = convert(line) if len(line) > 0 else None  # Corrected 'line' variable
+        line_2 = convert(line) if len(line) > 0 else None
         objl = []
         if line_2 is None:
             print("** class name missing **")
         elif line_2[0] not in self.__classes:
             print("** class doesn't exist **")
         else:
-<<<<<<< HEAD
-<<<<<<< HEAD
-            for ob in storage.all().values():
-=======
-            for ob in storege.all().values():
->>>>>>> parent of 29362e0... Added update method for the console app
-                if len(line_2) > 0 and argl[0] == obj.__class__.__name__:
-=======
             for ob in models.storage.all().values():
                 if line_2[0] == ob.__class__.__name__:
->>>>>>> d786d428a946a8ec02ee9945d6fd562b316e63ca
                     objl.append(ob.__str__())
                 elif len(line_2) == 0:
                     objl.append(ob.__str__())
         if len(objl) != 0:
             print(objl)
-<<<<<<< HEAD
-<<<<<<< HEAD
-        
-    def do_update(self, line):
-        """Update a class instance of a given id by adding or updating
-        a given attribute key/value pair or dictionary."""
-        line_2 = convert(line)
-        obj = storage.all()
-
-        if len(line_2) == 0:
-            print("** class name missing **")
-            return False
-        
-        if line_2[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
-            return False
-        
-        if len(line_2) == 1:
-            print("** instance id missing **")
-            return False
-
-        """if  "{}.{}".format(line_2[0], line_2[1]) not in obj.keys():
-            print("** no instance found **")
-            return False"""
-        
-        if len(line_2) == 2:
-            print("** attribute name missing **")
-            return False
-        
-        if len(line_2) == 3:
-            try:
-                type(eval(len_2[2])) != dict
-            except NameError:
-                print("** value missing **")
-                return False
-        
-        if len(line_2) == 4:
-            ob = obj["{}.{}".format(line_2[0], line_2[1])]
-            if line[2] in ob.__class__.__dict__.keys():
-                value_type = type(ob.__class__.__dict__[line_2[2]])
-                ob.__dict__[line_2[2]] = value_type(line_2[3])
-            else:
-                ob.__dict__[line_2[2]] = line_2[3]
-
-        storage.save()
-                    
-                    
-=======
 
 
                 
->>>>>>> d786d428a946a8ec02ee9945d6fd562b316e63ca
-=======
-                
->>>>>>> parent of 29362e0... Added update method for the console app
 
 
 if __name__ == '__main__':
