b1a2e970e0ec19c877238a63284643f3b98c28da
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-10-12 11:48:49 +0300

    Added class that inherit form BaseModel

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+"""A module for Amenity Class"""
+from models.base_model import BaseModel
+
+
+class Amenity:
+    """Represent an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
+    name = ""
