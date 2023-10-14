581624dd24659cb11592a5a98524deb4358951ab
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-10-14 18:57:52 +0300

    Updating amentiy to inherity from BaseModel

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 """A module for Amenity Class"""
 from models.base_model import BaseModel
 
 
-class Amenity:
+class Amenity(BaseModel):
     """Represent an amenity.
 
     Attributes:
