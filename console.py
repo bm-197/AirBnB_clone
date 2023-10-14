3c73c5d997d7b2da643d6f7b967fbb15fa5cfc41
ZorEl212 <yabsirad212@gmail.com>
2023-10-14 04:19:10 -0400

    Console: Improve input parsing for 'update' 	Also don't allow executing command if it is not comma separated. 	If all parameter are not separated with comma spit out default error Pycodestyle: Clean up console.py

--- console.py
+++ console.py
@@ -12,6 +12,7 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
+
 def convert(line):
     """Split input from tty in with whitespace as a delimiter
         Args:
@@ -20,6 +21,7 @@ def convert(line):
         return []
     line_2 = line.split(" ")
     return line_2
+
 
 class HBNBCommand(cmd.Cmd):
     """Defines the HolbertonBnB command interpreter."""
@@ -34,7 +36,6 @@ class HBNBCommand(cmd.Cmd):
         "Amenity": Amenity,
         "Review": Review}
 
-
     def do_quit(self, line):
         """Quit command to exit the progrma."""
         return True
@@ -47,32 +48,33 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing if no commnd is provided"""
         return
-    
+
     def do_create(self, line):
         """Create a new class instance and print its id."""
         line_2 = convert(line)
 
         if len(line_2) == 0:
             print("** class name missing **")
-        
+
         elif line_2[0] not in self.__classes:
             print("** class doesn't exit **")
-    
+
         print(self.__classes[line_2[0]]().id)
         models.storage.save()
-    
+
     def do_show(self, line):
-        """Display the string representation of a class instance of a given id."""
+        """Display the string representation of a class instance
+            of a given id."""
         line_2 = convert(line)
         obj = models.storage.all()
 
         if len(line_2) == 0:
             print("** class name missing **")
             return
-        
+
         elif line_2[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
-        
+
         elif len(line_2) == 1:
             print("** instance id missing **")
             return
@@ -80,7 +82,7 @@ class HBNBCommand(cmd.Cmd):
         elif "{}.{}".format(line_2[0], line_2[1]) not in obj.keys():
             print("** no instance found **")
         print(obj["{}.{}".format(line_2[0], line_2[1])])
-    
+
     def do_destroy(self, line):
         """Delete a class instance of a given id."""
         line_2 = convert(line)
@@ -88,10 +90,10 @@ class HBNBCommand(cmd.Cmd):
 
         if len(line_2) == 0:
             print("** class name missing **")
-        
+
         elif line_2[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
-        
+
         elif len(line_2) == 1:
             print("** instance id missing **")
 
@@ -101,7 +103,7 @@ class HBNBCommand(cmd.Cmd):
         else:
             del obj["{}.{}".format(line_2[0], line_2[1])]
             models.storage.save()
-    
+
     def do_all(self, line):
         """Display string representations of all instances of a given class."""
         line_2 = convert(line) if len(line) > 0 else None
@@ -128,30 +130,30 @@ class HBNBCommand(cmd.Cmd):
         if len(line_2) == 0:
             print("** class name missing **")
             return False
-        
+
         if line_2[0] not in self.__classes:
             print("** class doesn't exist **")
             return False
-        
+
         if len(line_2) == 1:
             print("** instance id missing **")
             return False
 
-        if  "{}.{}".format(line_2[0], line_2[1]) not in obj.keys():
+        if "{}.{}".format(line_2[0], line_2[1]) not in obj.keys():
             print("** no instance found **")
             return False
-        
+
         if len(line_2) == 2:
             print("** attribute name missing **")
             return False
-        
+
         if len(line_2) == 3:
             try:
                 type(eval(line_2[2])) != dict
             except NameError:
                 print("** value missing **")
                 return False
-        
+
         if len(line_2) == 4:
             ob = obj["{}.{}".format(line_2[0], line_2[1])]
             if line[2] in ob.__class__.__dict__.keys():
@@ -164,14 +166,14 @@ class HBNBCommand(cmd.Cmd):
 
     def default(self, line):
         line_split = line.split(".", maxsplit=1)
-    
+
         if len(line_split) != 2:
             super().default(line)
             return
-    
+
         class_name, command = line_split
         match = re.search(r"\((.*?)\)", command)
-    
+
         if match:
             command = [command[:match.span()[0]], match.group()[1: -1]]
             if hasattr(self, 'do_' + command[0]):
@@ -179,12 +181,12 @@ class HBNBCommand(cmd.Cmd):
                 call = class_name
                 if command[0] == "update":
                     args = ""
-                    for char in command[1]:
-                        if char != ",":
-                            args = args + str(char)
-                    call = f"{call} {args}"
-                    method(call)
-                    return
+                    args = ''.join(char for char in command[1] if char != ',')
+                    if args != command[1]:
+                        call = f"{call} {args}"
+                        method(call)
+                    else:
+                        super().default(line)
                 if len(command[1]) != 0:
                     call = f"{call} {command[1]}"
                 method(call)
@@ -197,6 +199,7 @@ class HBNBCommand(cmd.Cmd):
                 print(count)
         else:
             super().default(line)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
